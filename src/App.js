3561f0b7ee3eb48d69a070a49aa8ee1e48237ff9
Vinay Rao <vrao8912@gmail.com>
2022-11-23 14:17:12 +0100

    first comm

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/MyRecipes").then(({ data }) => {
+      console.log(" ", data);
+      setData(data);
+    });
+  }, []);
+
+  if (!data) return "Loading...";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App">{data?.entries[0].fields?.description["en-US"]}</div>
   );
 }
 
